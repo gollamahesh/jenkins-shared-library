ca54322ec95b496e2591588de0a7c02ea54bd50c
mahesh <maheshbabugolla9@gmail.com>
2023-10-27 21:41:24 -0400

    jenkins shared library

--- vars/pipelinedecision.groovy
+++ vars/pipelinedecision.groovy
@@ -1,4 +1,19 @@
 #!groovy
 def decidepipeline(Map configMap){
-    echo "I need to take decision map we set"
+    application = configMap.get("application")
+    //# here we are getting node js vm
+    switch(application) {
+        case 'nodeJSVM':
+            echo "application is nodejs vm"
+            //nodeJSVMCI(configMap) 
+            
+        break
+        case 'javaVM'
+            javaVMCI(configMap)
+        break
+        default:
+            error "unrecognized application"
+        break
+    }
+    //echo "I need to take decision map we set"
 }
